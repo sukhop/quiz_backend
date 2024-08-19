39b8f422645b8740945d47e576e4b01e00b5daf4
RichestsoftSukhbir <sukhbir.richestsoft@gmail.com>
2024-08-19 15:12:21 +0530

    working on auth

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -1,18 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 exports.authMiddleware = (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
+    const authHeader = req.headers.authorization;
+    
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+        const token = authHeader.split(" ")[1];
 
-    if(token) {
         jwt.verify(token, process.env.APP_SECRET, (err, decoded) => {
-            if(err) {
-                return res.json({err: "Invalid Token"})
+            if (err) {
+                return res.status(401).json({err: "Invalid Token"});
             }
             req.body.userId = decoded.userID;
             next();
         });
     } else {
-        console.log(token);
-        res.json({msg: "Login first"});
+        res.status(401).json({msg: "Login first"});
     }
-}
+};
